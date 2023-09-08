bc0dec4ee9df40aade1bb41fe1a9e1137dbd38fd
RaHuL-9714 <rahulgusai43@gmail.com>
2023-09-08 10:06:04 +0530

    dbConnectWithKnex

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,11 @@
 import express from 'express'
 import constant from './src/helpers/constant.js'
+import knex from './src/config/dbconfig.js'
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
 app.listen(constant.port,()=>{
     console.log(`Server is running on http://localhost:${constant.port}`)
