5c71c3e15ada6dd6b58974ad1a26738f42c884d9
RaHuL-9714 <rahulgusai43@gmail.com>
2023-09-19 09:28:48 +0530

    Error msg change

--- Backend/src/validation/blogs/blogs.js
+++ Backend/src/validation/blogs/blogs.js
@@ -5,7 +5,7 @@ const verifyBlog = Joi.object({
     "string.empty": `"Blog title" is a required field.`,
   }),
   description: Joi.string().min(120).max(1500).trim().required().messages({
-    "string.empty": `"Blog title" is a required field.`,
+    "string.empty": `"Blog description" is a required field.`,
     "string.length": `"Blog description" must have 120 words and maximum upto 1500 words`,
   }),
   category: Joi.string().min(1).max(30).required().messages({
